dda2fe89941209bf8fffb3bef56736207347acf8
Juan José Agudelo Vélez <109243184+Josefo22@users.noreply.github.com>
2025-02-08 22:24:19 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
     // Agrega aquí cualquier configuración adicional que uses
   };
   
